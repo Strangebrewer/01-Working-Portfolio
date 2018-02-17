b6e73502370e0edf4b7519606cfadd6e363676a4
Strangebrewer <BKAShambala@gmail.com>
2018-02-17 07:23:58 -0700

    added hamburger menu to smaller sizes

---
+++ assets/js/grid.js
@@ -0,0 +1,11 @@
+$("#example-one").on("click", function () {
+  $(".change").attr("id", "g1");
+});
+
+$("#example-two").on("click", function () {
+  $(".change").attr("id", "g2");
+});
+
+$("#example-three").on("click", function () {
+  $(".change").attr("id", "g3");
+});
