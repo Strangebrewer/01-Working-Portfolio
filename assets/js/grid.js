140ba420507d134c07bcfba83df913b80cdf2b57
Strangebrewer <BKAShambala@gmail.com>
2018-02-18 04:58:42 -0700

    finished grid examples

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -1,11 +1,41 @@
-$("#example-one").on("click", function () {
-  $(".change").attr("id", "g1");
-});
+// $("#example-one").on("click", function () {
+//   $(".change").attr("id", "g1");
+// });
 
-$("#example-two").on("click", function () {
-  $(".change").attr("id", "g2");
-});
+// $("#example-two").on("click", function () {
+//   $(".change").attr("id", "g2");
+// });
 
-$("#example-three").on("click", function () {
-  $(".change").attr("id", "g3");
+// $("#example-three").on("click", function () {
+//   $(".change").attr("id", "g3");
+// });
+
+moreDivsFlag = false;
+
+$(".grid-btn").on("click", function () {
+  if (moreDivsFlag === false) {
+    if ($(this).attr("narf") === "g4" || ($(this).attr("narf") === "g5" || $(this).attr("narf") === "g6")) {
+      $(".change").append($("<div class='box thirteen odd'>13</div>"));
+      $(".change").append($("<div class='box fourteen even'>14</div>"));
+      $(".change").append($("<div class='box fifteen odd'>15</div>"));
+      $(".change").attr("id", ($(this).attr("narf")));
+      moreDivsFlag = true;
+    }
+    else {
+      $(".change").attr("id", ($(this).attr("narf")));
+    }
+  }
+  else if (moreDivsFlag) {
+    var narfIs = $(this).attr("narf");
+    if (narfIs === "g1" || narfIs === "g2" || narfIs === "g3" || narfIs === "g8" || narfIs === "g9" || narfIs === "g10") {
+      $(".thirteen").remove();
+      $(".fourteen").remove();
+      $(".fifteen").remove();
+      $(".change").attr("id", ($(this).attr("narf")));
+      moreDivsFlag = false;
+    }
+    else {
+      $(".change").attr("id", ($(this).attr("narf")));
+    }
+  }
 });
